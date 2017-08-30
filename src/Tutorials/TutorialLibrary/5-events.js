78960a13f626ecaa3e9933720979d61c422a906c
BradfordMedeiros <bradmedeiros0@gmail.com>
2017-08-29 21:46:41 -0700

    added event logging

--- src/Tutorials/TutorialLibrary/5-events.js
+++ src/Tutorials/TutorialLibrary/5-events.js
@@ -1,6 +1,7 @@
+import React from 'react';
 
 export default {
-  title: 'Tutorial 5: Events and Alerting',
+  title: 'Tutorial 5: Events and Email Alerts',
   sections: [
 
     {
@@ -9,88 +10,55 @@ export default {
     },
     {
       type: 'exposition',
-      content: 'This tutorial will teach you how to install automate',
+      content: (
+        <div>
+          This tutorial will teach you about the concept of events, and what they can do.
+          <br/><br/>
+          After this tutorial you will be able to log events, view the history for an event, and set up email alerts.
+        </div>
+      ),
     },
-
     {
       type: 'space',
     },
     {
       type: 'overview',
-      content: 'Definitions',
+      content: 'Events',
+    },
+    {
+      type: 'exposition',
+      content:  (
+        <div>
+          An event is a mechanism in which can be used to log events of significance.  Events can be viewed in
+          the UI, as a way to generally view interesting occurences, and can be configured to set up email alerts.
+          <br /><br />
+          An event maps directly the any topic which is prefixed by event/(rest of event name.  For example, we can
+          send the combination - topic: events/garage/door, value: open, to specify a garage door opening, for example.
+          This will be viewable in the UI with time data attached.
+        </div>
+      )
     },
     {
       type: 'section',
-      content: 'Raspberry Pi Installation'
+      content: 'Logging an Event',
     },
     {
       type: 'definition',
-      name: 'Step 1',
-      content: 'Click here to download a copy of the raspberry pi image',
+      name: 'Step 1: Understanding the UI',
+      content: (
+        <div>
+          <div>To create a condition, simply up automate and navigate to the conditions tab</div>
+          <div>(add image here)</div>
+        </div>
+      ),
     },
     {
       type: 'definition',
-      name: 'Actions',
-      content: 'Actions are values corresponding to MQTT topics that are meant to trigger actions in the system. ' +
-      'For example, a motor might respond to the action actions/door/door_motor, and upon receiving the topic will open the door ' +
-      'if the value is on, else close the door.'
+      name: 'Step 2: Enter new code, and update',
+      content: 'Just like that, enter a value on the bar an press enter!'
     },
     {
-      type: 'definition',
-      name: 'Conditions',
-      content: 'Conditions are small scripts defined in javascript, that are used elsewhere in the system. ' +
-      'They are scripts that return true or false. They may access state information, or other information external to automate via http, or other method.'
-    },
-    {
-      type: 'section',
-      content: 'Linux Installation'
-    },
-    {
-      type: 'definition',
-      name: 'Rules',
-      content: 'A Rule is a task which runs in the background at a given interval.  ' +
-      'A rule is bound to a single condition, and will trigger based upon the edge type. When a rule is true, it is configured to send a message to a given mqtt topic.'
-    },
-    {
-      type: 'definition',
-      name: 'Sequences',
-      content: 'Sequences are specific orders of mqtt topics to send, with specification of time. ' +
-      'Sequences can be triggered by sending a message to the mqtttopic associated with the sequence.'
-    },
-    {
-      type: 'definition',
-      name: 'Schedules',
-      content: 'Schedules are a background task that will trigger a mqtt message to be sent at a given time or occurence (daily, weekly, etc - cron specified)'
-    },
-    {
-      type: 'section',
-      content: 'Windows Installation'
-    },
-    {
-      type: 'definition',
-      name: 'Events',
-      content: 'Events are triggers by sending and mqtt topic to /event/(rest of event name) with a value that will be considered the message body. ' +
-      'These can be used to log significant events of interest in the system, which can trigger email alerts and serve as a general history.'
-    },
-    {
-      type: 'definition',
-      name: 'ProtoProgrammer',
-      content: 'Refers to specific hardware that can be used to easily read or output voltage from a circuit and translate it into an mqtt topic, no programming required.'
-    },
-    {
-      type: 'section',
-      content: 'Building from source'
-    },
-    {
-      type: 'definition',
-      name: 'Events',
-      content: 'Events are triggers by sending and mqtt topic to /event/(rest of event name) with a value that will be considered the message body. ' +
-      'These can be used to log significant events of interest in the system, which can trigger email alerts and serve as a general history.'
-    },
-    {
-      type: 'definition',
-      name: 'ProtoProgrammer',
-      content: 'Refers to specific hardware that can be used to easily read or output voltage from a circuit and translate it into an mqtt topic, no programming required.'
+      type: 'space',
     },
     {
       type: 'space',
@@ -100,7 +68,7 @@ export default {
     },
     {
       type: 'overview',
-      content: 'General Information on Installation'
+      content: 'Video tutorial of Events'
     },
     {
       type: 'iframe',
